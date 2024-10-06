2d456bf022b66c133e363ce693480d43bfe571d8
LofiSoup <jariases.05@gmail.com>
2024-10-06 16:27:19 -0700

    rectangle code

--- draw_rectangle.pl
+++ draw_rectangle.pl
@@ -1,29 +1,48 @@
-draw_vertical_lines(Height, Width) :-
-    (Height == 0 -> true
-    ;   write('|'),
-        draw_symbol(' ', Width - 2),
-        write('|'),
-        nl,
-        Height1 is Height - 1,
-        draw_vertical_lines(Height1, Width)
-    ).
-draw_horizontal_line(Symbol, Width) :-
-    (Width == 0 -> nl
-    ;   write(Symbol),
-        Width1 is Width - 1,
-        draw_horizontal_line(Symbol, Width1)
-    ).
+% Draw N instances of Symbol
+drawSymbol(_, 0).
+drawSymbol(Symbol, N) :- 
+    N > 0, 
+    write(Symbol), 
+    N1 is N - 1, 
+    drawSymbol(Symbol, N1).
 
-draw_rectangle(LeftRightMargin, BottomTopMargin, SpaceBetweenCharacters, FontSize) :-
+% Draw a horizontal line of width N with the given Symbol
+drawHorizontalLine(Symbol, N) :- 
+    N > 0, 
+    write(Symbol), 
+    N1 is N - 1, 
+    drawHorizontalLine(Symbol, N1).
+drawHorizontalLine(_, 0) :- nl.
 
-    integer(LeftRightMargin),
-    integer(BottomTopMargin),
-    integer(SpaceBetweenCharacters),
+% Draw the vertical lines (with spaces between)
+drawVerticalLines(_, 0, _) :- !.
+drawVerticalLines(Symbol, Height, Width) :- 
+    Height > 0, 
+    write(Symbol),                     % Left edge
+    drawSymbol(' ', Width - 2),         % Spaces between
+    write(Symbol),                     % Right edge
+    nl,
+    Height1 is Height - 1, 
+    drawVerticalLines(Symbol, Height1, Width).
+
+% Draw the rectangle
+drawRectangle(LeftRightMargin, BottomTopMargin, SpaceBetweenCharacters, FontSize) :- 
+    integer(LeftRightMargin), 
+    integer(BottomTopMargin), 
+    integer(SpaceBetweenCharacters), 
     integer(FontSize),
-    Width is (LeftRightMargin * 2) + (SpaceBetweenCharacters * 2) + (FontSize * 3 * 3) + 2,
-    Height is (BottomTopMargin * 2) + (FontSize * 5),
-    draw_horizontal_line(Hyphen, Width), 
-    nl, 
-    draw_vertical_lines(Height, Width),
-    draw_horizontal_line(Hyphen, Width)
-men
+    
+    % Width calculation based on margins, font size, and space
+    Width is LeftRightMargin * 2 + SpaceBetweenCharacters + FontSize * 3,
+    
+    % Height calculation: simply based on margins and font size for clarity
+    Height is BottomTopMargin * 2 + FontSize,
+    
+    % Draw top border
+    drawHorizontalLine('-', Width),
+    
+    % Draw vertical lines
+    drawVerticalLines('|', Height, Width),
+    
+    % Draw bottom border
+    drawHorizontalLine('-', Width).
